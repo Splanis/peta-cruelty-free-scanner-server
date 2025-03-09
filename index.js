d064271af2adb872586c03fdbc981e7c83e44589
Splanis <m.galanis@ferryhopper.com>
2025-03-09 17:20:46 +0200

    das

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ app.get('/check-brand/:brand', async (req, res) => {
     const brand = req.params.brand; // Get brand from URL
     if (!brand) return res.status(400).json({ error: 'Brand name is required' });
 
-    const browser = await puppeteer.launch({
-        args: ['--disable-setuid-sandbox', '--no-sandbox', '--single-process', '--no-zygote'],
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-    });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.setUserAgent(
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
